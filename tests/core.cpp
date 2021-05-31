4fd70e4d41cd835795a8356fec16b13903f87ed2
Dominik Drexler <dominik.drexler@liu.se>
2021-05-31 16:17:08 +0200

    added some more atoms to test

--- tests/core.cpp
+++ tests/core.cpp
@@ -7,13 +7,17 @@ TEST(DLPTests, InstanceCreation) {
     dlp::core::InstanceInfo instance;
     dlp::core::Atom a0 = instance.add_atom("on", {"A", "B"}, false);
     dlp::core::Atom a1 = instance.add_atom("on", {"B", "A"}, false);
+    dlp::core::Atom a2 = instance.add_atom("onTable", {"A"}, false);
+    dlp::core::Atom a3 = instance.add_atom("onTable", {"B"}, false);
+    dlp::core::Atom a4 = instance.add_atom("holding", {"A"}, false);
+    dlp::core::Atom a5 = instance.add_atom("holding", {"B"}, false);
+    dlp::core::Atom a6 = instance.add_atom("on_g", {"A", "B"}, true);
 
     dlp::core::SyntacticElementFactory factory;
 
     dlp::core::NumericalElement numerical = factory.parse_numerical_element("n_count(c_and(on_g(0),on(0)))");
     EXPECT_EQ(numerical.complexity(), 4);
 
-    // TODO This raises some errors?
-//    dlp::core::State state = instance.convert_state({0, 3});
-//    int value = numerical.evaluate(state);
+    dlp::core::State state = instance.convert_state({a0, a1});
+    int value = numerical.evaluate(state);
 }
