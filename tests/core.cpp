3a4bb644d081e4d5844bb253132c796b8e68ff5a
Guillem Frances <guillem.frances@gmail.com>
2021-06-02 09:29:30 +0200

    Fix and expand test

--- tests/core.cpp
+++ tests/core.cpp
@@ -1,41 +1,56 @@
 
 #include <gtest/gtest.h>
-#include "dlp/core.h"
+
+#include <dlp/core.h>
+
+using namespace dlp::core;
+
+
+TEST(DLPTests, SyntacticErrorHandling) {
+    VocabularyInfo vocabulary;
+    auto clear = vocabulary.add_predicate("on", 2);
+
+    SyntacticElementFactory factory(vocabulary);
+
+    ASSERT_THROW(factory.parse_numerical("hello world!"), std::runtime_error);
+    ASSERT_THROW(factory.parse_concept("bye world!"), std::runtime_error);
+
+    ASSERT_THROW(factory.parse_concept("c_sand(on(0),on_g(0))"), std::runtime_error);
+    ASSERT_THROW(factory.parse_concept("or(on(0),on_g(0))"), std::runtime_error);
+    ASSERT_THROW(factory.parse_concept("c_and(on(0), on(0), on(0))"), std::runtime_error);
+
+    ASSERT_THROW(factory.parse_numerical("n_count(on)"), std::runtime_error);
+    ASSERT_THROW(factory.parse_numerical("n_count(on(4))"), std::runtime_error);
+    ASSERT_THROW(factory.parse_numerical("n_count(on(-1))"), std::runtime_error);
+    ASSERT_THROW(factory.parse_numerical("n_count(on_g(0))"), std::runtime_error);
+}
+
 
 // Demonstrate some basic assertions.
 TEST(DLPTests, InstanceCreation) {
     // Add predicates
-    dlp::core::VocabularyInfo vocabulary;
-    dlp::core::Predicate p0 = vocabulary.add_predicate("on", 2);
-    dlp::core::Predicate p1 = vocabulary.add_predicate("onTable", 1);
-    dlp::core::Predicate p2 = vocabulary.add_predicate("holding", 1);
-    dlp::core::Predicate p3 = vocabulary.add_predicate("on_g", 2);
-    std::vector<dlp::core::Predicate> predicates({p0, p1, p2, p3});
-    dlp::core::InstanceInfo instance(vocabulary);
+    VocabularyInfo vocabulary;
+    Predicate p0 = vocabulary.add_predicate("on", 2);
+    Predicate p1 = vocabulary.add_predicate("onTable", 1);
+    Predicate p2 = vocabulary.add_predicate("holding", 1);
+    Predicate p3 = vocabulary.add_predicate("on_g", 2);
+    std::vector<Predicate> predicates({p0, p1, p2, p3});
+    InstanceInfo instance(vocabulary);
     // Add state atoms
-    dlp::core::Atom a0 = instance.add_atom("on", {"A", "B"});
-    dlp::core::Atom a1 = instance.add_atom("on", {"B", "A"});
-    dlp::core::Atom a2 = instance.add_atom("onTable", {"A"});
-    dlp::core::Atom a3 = instance.add_atom("onTable", {"B"});
-    dlp::core::Atom a4 = instance.add_atom("holding", {"A"});
-    dlp::core::Atom a5 = instance.add_atom("holding", {"B"});
+    Atom a0 = instance.add_atom("on", {"A", "B"});
+    Atom a1 = instance.add_atom("on", {"B", "A"});
+    Atom a2 = instance.add_atom("onTable", {"A"});
+    Atom a3 = instance.add_atom("onTable", {"B"});
+    Atom a4 = instance.add_atom("holding", {"A"});
+    Atom a5 = instance.add_atom("holding", {"B"});
     // Add goal atoms
-    dlp::core::Atom a6 = instance.add_static_atom("on_g", {"A", "B"});
+    Atom a6 = instance.add_static_atom("on_g", {"A", "B"});
 
-    dlp::core::SyntacticElementFactory factory(vocabulary);
+    SyntacticElementFactory factory(vocabulary);
 
-    dlp::core::Numerical numerical = factory.parse_numerical("n_count(c_and(on_g(0),on(0)))");
+    Numerical numerical = factory.parse_numerical("n_count(c_and(on_g(0),on(0)))");
     EXPECT_EQ(numerical.compute_complexity(), 4);
 
-    dlp::core::State state = instance.convert_state({a0, a1});
+    State state = instance.convert_state({a0, a1});
     EXPECT_EQ(numerical.evaluate(state), 1);
-}
-
-TEST(DLPTests, CreateSConcept) {
-    dlp::core::VocabularyInfo vocabulary;
-    dlp::core::Predicate p0 = vocabulary.add_predicate("clear", 2);
-    dlp::core::SyntacticElementFactory factory(vocabulary);
-
-    auto numerical = factory.parse_numerical("n_count(clear)");
-    EXPECT_EQ(numerical.compute_complexity(), 2);
 }
