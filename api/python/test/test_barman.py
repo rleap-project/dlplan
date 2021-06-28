0f78923e4540eeca4016bcb33a09596c28fd2a0e
Dominik Drexler <dominik.drexler@liu.se>
2021-06-28 20:43:36 +0200

    added test for barman

---
+++ api/python/test/test_barman.py
@@ -0,0 +1,67 @@
+
+from dlplan import VocabularyInfo, InstanceInfo, SyntacticElementFactory, State
+
+
+def generate_barman_vocabulary():
+    vocabulary = VocabularyInfo()
+    vocabulary.add_predicate("ontable", 1)
+    vocabulary.add_predicate("holding", 2)
+    vocabulary.add_predicate("handempty", 1)
+    vocabulary.add_predicate("empty", 1)
+    vocabulary.add_predicate("contains", 2)
+    vocabulary.add_predicate("contains_g", 2)
+    vocabulary.add_predicate("clean", 1)
+    vocabulary.add_predicate("used", 2)
+    vocabulary.add_predicate("dispenses", 2)
+    vocabulary.add_predicate("shaker-empty-level", 2)
+    vocabulary.add_predicate("shaker-level", 2)
+    vocabulary.add_predicate("next", 2)
+    vocabulary.add_predicate("unshaked", 1)
+    vocabulary.add_predicate("shaked", 1)
+    vocabulary.add_predicate("cocktail-part1", 2)
+    vocabulary.add_predicate("cocktail-part2", 2)
+    return vocabulary
+
+
+def generate_barman_instance(vocabulary):
+    # {shaker-level(shaker1,l1), contains(shaker1,ingredient2), unshaked(shaker1), empty(shot1),
+    # used(shot1,ingredient2), holding(left,shot1), holding(right,shaker1), contains_g(shot1,cocktail1)}
+    instance = InstanceInfo(vocabulary)
+    instance.add_atom("shaker-level", ["shaker1", "l1"])
+    instance.add_atom("contains", ["shaker1", "ingredient2"])
+    instance.add_atom("unshaked", ["shaker1"])
+    instance.add_atom("empty", ["shot1"])
+    instance.add_atom("used", ["shot1", "ingredient2"])
+    instance.add_atom("holding", ["left", "shot1"])
+    instance.add_atom("holding", ["right", "shaker1"])
+    # static atoms, only partially added for now
+    instance.add_static_atom("cocktail-part1", ["cocktail1", "ingredient2"])
+    instance.add_static_atom("cocktail-part2", ["cocktail1", "ingredient1"])
+    instance.add_static_atom("contains_g", ["shot1", "cocktail1"])
+    return instance
+
+
+def test_core_instance():
+    """ Exhaustively test InstanceInfo
+    """
+    vocabulary = generate_barman_vocabulary()
+    instance = generate_barman_instance(vocabulary)
+    factory = SyntacticElementFactory(vocabulary)
+    state = State(instance, instance.get_atoms())
+    concept = factory.parse_concept("shaker-level(0)")
+    result = concept.evaluate(state)
+    assert result == [0]
+    assert instance.get_object(result[0]).get_name() == "shaker1"
+    concept = factory.parse_concept("c_some(contains(1,0),shaker-level(0))")
+    result = concept.evaluate(state)
+    assert result == [2]
+    assert instance.get_object(result[0]).get_name() == "ingredient2"
+    role = factory.parse_role("cocktail-part1(0,1)")
+    result = role.evaluate(state)
+    assert result == [(6,2)]
+    concept = factory.parse_concept("c_some(cocktail-part1(0,1),c_some(contains(1,0),shaker-level(0)))")
+    result = concept.evaluate(state)
+    assert result == [6]
+    assert instance.get_object(result[0]).get_name() == "cocktail1"
+    numerical = factory.parse_numerical("n_count(c_and(c_some(cocktail-part1(0,1),c_some(contains(1,0),shaker-level(0))),c_projection(r_diff(contains_g(0,1),contains(0,1)),1)))")
+    assert numerical.evaluate(state) == 1
