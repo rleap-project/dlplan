a75e8205f1c82933da5af96127d62f9c72d223de
Dominik Drexler <dominik.drexler@liu.se>
2021-09-01 17:40:12 +0200

    moved fallthrough declarations

--- src/utils/MurmurHash3.cpp
+++ src/utils/MurmurHash3.cpp
@@ -129,10 +129,13 @@ void MurmurHash3_x86_32 ( const void * key, int len,
 
   switch(len & 3)
   {
-  case 3: k1 ^= tail[2] << 16; [[fallthrough]];
-  case 2: k1 ^= tail[1] << 8; [[fallthrough]];
+  case 3: k1 ^= tail[2] << 16;
+  [[fallthrough]];
+  case 2: k1 ^= tail[1] << 8;
+  [[fallthrough]];
   case 1: k1 ^= tail[0];
-          k1 *= c1; k1 = ROTL32(k1,15); k1 *= c2; h1 ^= k1; [[fallthrough]];
+          k1 *= c1; k1 = ROTL32(k1,15); k1 *= c2; h1 ^= k1;
+  [[fallthrough]];
   };
 
   //----------
