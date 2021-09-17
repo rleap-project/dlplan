7cbba173846329bc1d993639047e7b0bac394a35
Dominik Drexler <dominik.drexler@liu.se>
2021-09-17 17:42:48 +0200

    fixed evaluation bug when using multiple instances where the number of objects differs

--- src/core/elements/roles/top.h
+++ src/core/elements/roles/top.h
@@ -10,20 +10,26 @@ class TopRole : public Role {
 private:
     mutable RoleDenotation m_result;
 
+    // Store a weak reference to the InstanceInfo of the previously evaluated state to avoid recomputation.
+    mutable std::weak_ptr<const InstanceInfo> m_evaluation_instance;
+
 public:
     TopRole(const VocabularyInfo& vocabulary)
     : Role(vocabulary, "r_top"){
     }
 
     RoleDenotation evaluate(const State& state) const override {
-        if (m_result.empty()) {
+        std::shared_ptr<const InstanceInfo> instance = state.get_instance_info();
+        if (instance != m_evaluation_instance.lock()) {
             int num_objects = state.get_instance_info()->get_num_objects();
+            m_result.clear();
             m_result.reserve(num_objects * num_objects);
             for (int object_idx_1 = 0; object_idx_1 < num_objects; ++object_idx_1) {
                 for (int object_idx_2 = 0; object_idx_2 < num_objects; ++object_idx_2) {
                     m_result.emplace_back(object_idx_1, object_idx_2);
                 }
             }
+            m_evaluation_instance = instance;
         }
         return m_result;
     }
