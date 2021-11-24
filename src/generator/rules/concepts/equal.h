d493af1ece5193bfff5d114ad7006f73519f839b
Dominik Drexler <dominik.drexler@gmx.net>
2021-11-24 13:32:31 +0100

    commented out additional restriction on c_equal

--- src/generator/rules/concepts/equal.h
+++ src/generator/rules/concepts/equal.h
@@ -12,19 +12,24 @@ public:
     virtual void generate(const States& states, int iteration, FeatureGeneratorData& data) override {
         if (iteration == 2) {
             for (const auto& role_left : data.get_role_elements_by_complexity()[1]) {
+                // Some further restriction used in D2L where righthandside can only be goal version of left handside.
+                // However, this the EqualConcept does not contribute much to the blowup.
+                /*
                 int pos1 = 12;  // len("r_primitive(") == 12
                 std::string repr1 = role_left.compute_repr();
                 int pos2 = repr1.find(',');
-                for (const auto& role_right : data.get_role_elements_by_complexity()[1]) {                    
-                    std::string repr2 = role_right.compute_repr();
+                */
+                for (const auto& role_right : data.get_role_elements_by_complexity()[1]) { 
                     // TODO: this is extremely ugly. The alternative is to provide getters, downcast to PrimitiveRole and check using get_name method.
+                    /*
+                    std::string repr2 = role_right.compute_repr();
                     if (repr1.size() < 12 
                         || repr2.size() < 12
                         || repr1.substr(0,12) != "r_primitive("
                         || repr2.substr(0,12) != "r_primitive(" 
                         || repr1.substr(pos1, pos2-pos1) + "_g" != repr2.substr(pos1, pos2-pos1+2)) {
                             continue;
-                    }
+                    }*/
                     if (data.reached_limit()) return;
                     else if (data.add_concept(states, data.get_factory().make_equal_concept(role_left, role_right))) {
                         m_count_instantiations += 1;
