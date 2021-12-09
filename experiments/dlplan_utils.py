d2780f273009e0e9fc20dc9895a6d48ec60d168e
Dominik Drexler <dominik.drexler@liu.se>
2021-12-09 20:00:07 +0100

    added state space exploration via tarski to experiments

--- experiments/dlplan_utils.py
+++ experiments/dlplan_utils.py
@@ -21,23 +21,45 @@ def construct_vocabulary_info(domain_data):
 def construct_instance_info(domain_data, instance_data):
     """ Constructs an InstanceInfo from a problem description. """
     instance_info = dlplan.InstanceInfo(domain_data.vocabulary_info)
+    # Add dynamic atoms
+    tarski_atom_to_dlplan_atom = dict()
+    for tarski_atom in instance_data.dynamic_atoms:
+        dlplan_atom = instance_info.add_atom(tarski_atom.predicate.name, [obj.name for obj in tarski_atom.subterms])
+        assert tarski_atom not in tarski_atom_to_dlplan_atom
+        tarski_atom_to_dlplan_atom[tarski_atom] = dlplan_atom
+    # Add other static atoms
+    for static_atom in instance_data.static_atoms:
+        instance_info.add_static_atom(static_atom.predicate.name, [obj.name for obj in static_atom.subterms])
     # Add static goal atoms
     for tarski_atom in instance_data.tarski_goal_atoms:
         predicate_name = tarski_atom.predicate.name
         object_names = []
-        for object in tarski_atom.subterms:
-            object_names.append(object.name)
+        for obj in tarski_atom.subterms:
+            object_names.append(obj.name)
         # add atom as goal version of the predicate
         instance_info.add_static_atom(predicate_name + "_g", object_names)
-    # Adddynamic atoms
-    for atom in instance_data.transition_system.dynamic_atoms:
-        instance_info.add_atom(atom.predicate.name, [object.name for object in atom.objects])
-    # Add other static atoms
-    for static_atom in instance_data.transition_system.static_atoms:
-        instance_info.add_static_atom(static_atom.predicate.name, [object.name for object in static_atom.objects])
-    return instance_info
+    return instance_info, tarski_atom_to_dlplan_atom
 
 
 def construct_syntactic_element_factory(domain_data):
     """ Constructs an empty factory for constructing elements. """
     return dlplan.SyntacticElementFactory(domain_data.vocabulary_info)
+
+
+def construct_feature_generator():
+    feature_generator = dlplan.FeatureGenerator()
+    feature_generator.set_generate_diff_concept(False)
+    feature_generator.set_generate_or_concept(False)
+    feature_generator.set_generate_subset_concept(False)
+    feature_generator.set_generate_role_distance_numerical(False)
+    feature_generator.set_generate_sum_concept_distance_numerical(False)
+    feature_generator.set_generate_sum_role_distance_numerical(False)
+    feature_generator.set_generate_and_role(False)
+    feature_generator.set_generate_compose_role(False)
+    feature_generator.set_generate_diff_role(False)
+    feature_generator.set_generate_identity_role(False)
+    feature_generator.set_generate_not_role(False)
+    feature_generator.set_generate_or_role(False)
+    feature_generator.set_generate_top_role(False)
+    feature_generator.set_generate_transitive_reflexive_closure_role(False)
+    return feature_generator
--- experiments/instance_data.py
+++ experiments/instance_data.py
@@ -1,5 +1,10 @@
+from os import stat
 import tarski
 from tarski.io import PDDLReader
+from tarski.grounding import LPGroundingStrategy
+from tarski.grounding.lp_grounding import ground_problem_schemas_into_plain_operators
+from tarski.search import GroundForwardSearchModel
+from dlplan_utils import construct_instance_info
 
 
 class InstanceData:
@@ -8,28 +13,55 @@ class InstanceData:
         self.instance_file = instance_file
         self.domain_data = domain_data
         # PDDL information
-        problem = self._parse_instance_file(domain_data.domain_file, instance_file)
-        self.tarski_goal_atoms = self._parse_conjunctive_formula(problem.goal)
-        # Store other instance related data here, e.g., transition system, instance_info
-        self.transition_system = None
-        self.instance_info = None
+        self.problem = parse_instance_file(domain_data.domain_file, instance_file)
+        grounder = LPGroundingStrategy(self.problem)
+        # Note: we transform tarski.StateVariableLite back to Atoms here.
+        self.dynamic_atoms = set(tarski.syntax.Atom(state_variable_lite.symbol, state_variable_lite.binding)
+                                 for state_variable_lite in grounder.ground_state_variables().data.keys())
+        self.static_atoms = [tarski.syntax.Atom(state_variable_lite.symbol, state_variable_lite.binding)
+                             for state_variable_lite in grounder.ground_static_variables().data.keys()]
 
-    def _parse_instance_file(self, domain_file, instance_file):
-        """ Parses the PDDL instance file using Tarski. """
-        reader = PDDLReader()
-        reader.parse_domain(domain_file)
-        reader.parse_instance(instance_file)
-        return reader.problem
+        problem = parse_instance_file(domain_data.domain_file, instance_file)
+        self.tarski_goal_atoms = parse_conjunctive_formula(problem.goal)
+        self.instance_info, self.tarski_atom_to_dlplan_atom = construct_instance_info(domain_data, self)
+        self.search_model = GroundForwardSearchModel(self.problem, ground_problem_schemas_into_plain_operators(self.problem))
+        self.states = state_space_exploration(self.problem, self.search_model)
+        print(self.states)
 
-    def _parse_conjunctive_formula(self, goal):
-        """ Compute all tarski Atoms from a tarski ComboundFormula. """
-        if isinstance(goal, tarski.syntax.formulas.CompoundFormula):
-            if goal.connective == tarski.syntax.formulas.Connective.And:
-                atom_names = []
-                for subformula in goal.subformulas:
-                    atom_names.extend(self._parse_conjunctive_formula(subformula))
-                return atom_names
-            else:
-                raise Exception(f"Unsupported connective {goal.connective} in goal description.")
-        elif isinstance(goal, tarski.syntax.formulas.Atom):
-            return [goal]
+
+def parse_instance_file(domain_file, instance_file):
+    """ Parses the PDDL instance file using Tarski. """
+    reader = PDDLReader()
+    reader.parse_domain(domain_file)
+    reader.parse_instance(instance_file)
+    return reader.problem
+
+
+def parse_conjunctive_formula(goal):
+    """ Compute all tarski Atoms from a tarski ComboundFormula. """
+    if isinstance(goal, tarski.syntax.formulas.CompoundFormula):
+        if goal.connective == tarski.syntax.formulas.Connective.And:
+            atom_names = []
+            for subformula in goal.subformulas:
+                atom_names.extend(parse_conjunctive_formula(subformula))
+            return atom_names
+        else:
+            raise Exception(f"Unsupported connective {goal.connective} in goal description.")
+    elif isinstance(goal, tarski.syntax.formulas.Atom):
+        return [goal]
+
+
+def state_space_exploration(problem, search_model):
+    """ Run DFS to explore state space. """
+    frontier = []
+    frontier.append(problem.init)
+    generated = set()
+    generated.add(problem.init)
+    while frontier:
+        cur = frontier.pop()
+        successors = search_model.successors(cur)
+        for action, succ in successors:
+            if succ in generated: continue
+            frontier.append(succ)
+            generated.add(succ)
+    return [model.as_atoms() for model in generated]
