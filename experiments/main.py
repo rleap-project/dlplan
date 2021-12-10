0b1a7a17a5a7f1d29407878f2d34ff59e7289bac
Dominik Drexler <dominik.drexler@gmx.net>
2021-12-10 10:07:01 +0100

    added feature generator arguments to argparse

--- experiments/main.py
+++ experiments/main.py
@@ -14,17 +14,19 @@ from transition_system_generator import TransitionSystemGenerator
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Example on how to initialize VocabularyInfo, InstanceInfo, SyntacticElementFactory from PDDL files.')
+    parser = argparse.ArgumentParser(description='Generating features for a single PDDL instance.')
     parser.add_argument("--domain", type=str, required=True)
     parser.add_argument("--instance", type=str, required=True)
-    # parser.add_argument("--path_to_sse", type=str, required=True)
+    parser.add_argument("--c", type=int, required=False, default=5)
+    parser.add_argument("--t", type=int, required=False, default=1800)
+    parser.add_argument("--f", type=int, required=False, default=1000000)
     args = parser.parse_args()
 
     domain_data = DomainData(args.domain)
     instance_data = InstanceData(args.instance, domain_data)
     dlplan_states = [state.dlplan_state for state in instance_data.states]
 
-    domain_data.feature_generator.generate(domain_data.syntactic_element_factory, 20, 1800, 100000, dlplan_states)
+    domain_data.feature_generator.generate(domain_data.syntactic_element_factory, args.c, args.t, args.f, dlplan_states)
 
     print("Instance information:")
     print(f"Number of states: {len(dlplan_states)}")
