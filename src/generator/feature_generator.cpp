bb012cb7d892afdd647ab3a17bc8df0477639ee5
Dominik Drexler <dominik.drexler@gmx.net>
2022-01-04 19:05:43 +0100

    version with fine grained tasks

--- src/generator/feature_generator.cpp
+++ src/generator/feature_generator.cpp
@@ -162,7 +162,6 @@ void FeatureGeneratorImpl::generate_inductively(int complexity, const States& st
             rule->parse_results_of_tasks(iteration, data);
         }
         // TODO(dominik): sleep main thread until queue is empty.
-        std::cout << "generated tasks for iteration: " << iteration << std::endl;
         while (!th.get_queue().empty()) { }
         utils::g_log << "Complexity " << iteration+1 << ":" << std::endl;
         data.print_statistics();
