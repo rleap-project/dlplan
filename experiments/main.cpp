9fc375a5298b948b8c200cf0bdef1a73f0ab3037
Dominik Drexler <dominik.drexler@gmx.net>
2022-02-14 18:44:02 +0100

    state parsing

--- experiments/main.cpp
+++ experiments/main.cpp
@@ -134,37 +134,55 @@ dlplan::core::SyntacticElementFactory construct_factory(
     return dlplan::core::SyntacticElementFactory(vocabulary_info);
 }
 
+enum class StateTokenType {
+    COMMA,
+    OPENING_BRACE,
+    CLOSING_BRACE,
+    IDENTIFIER
+};
+
+static const std::vector<std::pair<StateTokenType, std::regex>> state_token_type_expressions = {
+    { StateTokenType::COMMA, build_regex(",") },
+    { StateTokenType::OPENING_BRACE, build_regex("\\{") },
+    { StateTokenType::CLOSING_BRACE, build_regex("\\}") },
+    { StateTokenType::IDENTIFIER, build_regex("[a-zA-Z_\\-\\(\\)]\\w*") },
+};
+
 dlplan::core::State parse_state_line(
     std::shared_ptr<const dlplan::core::InstanceInfo> instance_info,
     const std::string& line) {
     auto start = line.begin();
     const auto end = line.end();
     std::smatch match;
-    std::regex_search(start, end, match, std::regex("(.*)\\("));
-    std::string predicate_name = match[1].str();
-    start += match[0].str().size();
-    std::vector<std::string> object_names;
-    // {at(tray1,kitchen), at(tray3,kitchen), at(tray2,kitchen), at(tray4,kitchen), at_kitchen_bread(bread19), at_kitchen_bread(bread18), at_kitchen_bread(bread14), at_kitchen_bread(bread8), at_kitchen_bread(bread10), at_kitchen_bread(bread2), at_kitchen_bread(bread9), at_kitchen_bread(bread16), at_kitchen_bread(bread20), at_kitchen_bread(bread17), at_kitchen_bread(bread7), at_kitchen_bread(bread24), at_kitchen_bread(bread12), at_kitchen_bread(bread3), at_kitchen_bread(bread22), at_kitchen_bread(bread13), at_kitchen_bread(bread11), at_kitchen_bread(bread5), at_kitchen_bread(bread6), at_kitchen_bread(bread21), at_kitchen_bread(bread15), at_kitchen_bread(bread1), at_kitchen_bread(bread23), at_kitchen_content(content22), at_kitchen_content(content14), at_kitchen_content(content6), at_kitchen_content(content19), at_kitchen_content(content13), at_kitchen_content(content20), at_kitchen_content(content23), at_kitchen_content(content2), at_kitchen_content(content8), at_kitchen_content(content10), at_kitchen_content(content16), at_kitchen_content(content15), at_kitchen_content(content1), at_kitchen_content(content12), at_kitchen_content(content3), at_kitchen_content(content17), at_kitchen_content(content11), at_kitchen_content(content4), at_kitchen_content(content5), at_kitchen_content(content9), at_kitchen_content(content18), at_kitchen_content(content7), at_kitchen_content(content24), notexist(sandw27), notexist(sandw18), notexist(sandw21), notexist(sandw32), notexist(sandw9), notexist(sandw13), notexist(sandw20), notexist(sandw25), notexist(sandw28), notexist(sandw11), notexist(sandw4), notexist(sandw8), notexist(sandw19), notexist(sandw29), notexist(sandw14), notexist(sandw17), notexist(sandw5), notexist(sandw24), notexist(sandw15), notexist(sandw16), notexist(sandw1), notexist(sandw22), notexist(sandw30), notexist(sandw3), notexist(sandw6), notexist(sandw23), notexist(sandw10), notexist(sandw7), notexist(sandw31), notexist(sandw12), notexist(sandw26), at_kitchen_sandwich(sandw2), no_gluten_sandwich(sandw2), not_allergic_gluten(child12), not_allergic_gluten(child21), not_allergic_gluten(child23), not_allergic_gluten(child16), not_allergic_gluten(child10), not_allergic_gluten(child17), not_allergic_gluten(child4), not_allergic_gluten(child18), not_allergic_gluten(child19), not_allergic_gluten(child24), not_allergic_gluten(child13), not_allergic_gluten(child3), not_allergic_gluten(child20), not_allergic_gluten(child7), not_allergic_gluten(child1), allergic_gluten(child9), allergic_gluten(child22), allergic_gluten(child6), allergic_gluten(child8), allergic_gluten(child5), allergic_gluten(child14), allergic_gluten(child15), allergic_gluten(child2), allergic_gluten(child11), no_gluten_bread(bread13), no_gluten_bread(bread4), no_gluten_bread(bread18), no_gluten_bread(bread20), no_gluten_bread(bread23), no_gluten_bread(bread5), no_gluten_bread(bread22), no_gluten_bread(bread14), no_gluten_bread(bread10), no_gluten_content(content7), no_gluten_content(content14), no_gluten_content(content2), no_gluten_content(content21), no_gluten_content(content4), no_gluten_content(content5), no_gluten_content(content22), no_gluten_content(content17), no_gluten_content(content8), waiting(child3,table2), waiting(child23,table2), waiting(child6,table2), waiting(child5,table2), waiting(child16,table1), waiting(child22,table3), waiting(child12,table1), waiting(child11,table2), waiting(child1,table2), waiting(child18,table1), waiting(child13,table3), waiting(child9,table2), waiting(child24,table2), waiting(child7,table3), waiting(child20,table1), waiting(child8,table1), waiting(child2,table3), waiting(child10,table2), waiting(child4,table2), waiting(child19,table1), waiting(child21,table3), waiting(child17,table1), waiting(child14,table1), waiting(child15,table2), served_g(child1), served_g(child2), served_g(child3), served_g(child4), served_g(child5), served_g(child6), served_g(child7), served_g(child8), served_g(child9), served_g(child10), served_g(child11), served_g(child12), served_g(child13), served_g(child14), served_g(child15), served_g(child16), served_g(child17), served_g(child18), served_g(child19), served_g(child20), served_g(child21), served_g(child22), served_g(child23), served_g(child24)}
-
+    std::vector<std::pair<StateTokenType, std::string>> tokens;
     while (start != end) {
         bool has_match = false;
-        for (const auto& regex : {std::regex("(.*),"), std::regex("(.*)\\)")}) {
+        for (const auto& pair : state_token_type_expressions) {
+            std::regex regex = pair.second;
             if (std::regex_search(start, end, match, regex)) {
-                object_names.push_back(match[1].str());
+                tokens.emplace_back(pair.first, match[1].str());
                 start += match[0].str().size();
                 has_match = true;
             }
         }
         if (!has_match) {
-            std::runtime_error("parse_atom_line - cannot parse line into atom. Line is: " + line);
+            throw std::runtime_error("parse_state_line - cannot parse line into state. Line is: " + line);
         }
     }
+    //for (const auto& pair : tokens) {
+    //    std::cout << pair.second << " ";
+    //}
+    //std::cout << std::endl;
+    return dlplan::core::State(instance_info, std::vector<int>());
 }
 
 std::vector<dlplan::core::State> construct_states(
     std::shared_ptr<const dlplan::core::InstanceInfo> instance_info,
     std::vector<std::string> states_lines) {
     std::vector<dlplan::core::State> states;
+    for (const auto& line : states_lines) {
+        states.push_back(parse_state_line(instance_info, line));
+    }
     return states;
 }
 
