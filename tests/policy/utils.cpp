3d15d6f2d305a9cbb1258336f2cf7d1210655b48
Dominik Drexler <dominik.drexler@gmx.net>
2022-09-05 15:23:38 +0200

    work on tests

--- tests/policy/utils.cpp
+++ tests/policy/utils.cpp
@@ -2,9 +2,23 @@
 
 std::shared_ptr<const VocabularyInfo> construct_vocabulary_info() {
     std::shared_ptr<VocabularyInfo> vocabulary_info = std::make_shared<VocabularyInfo>();
-    vocabulary_info->add_predicate("role", 2);
-    vocabulary_info->add_predicate("concept", 1);
+    vocabulary_info->add_predicate("at", 2);
+    vocabulary_info->add_predicate("package", 1);
     return vocabulary_info;
+}
+
+std::shared_ptr<const InstanceInfo> construct_instance_info(std::shared_ptr<const VocabularyInfo> vocabulary_info) {
+    std::shared_ptr<InstanceInfo> instance_info = std::make_shared<InstanceInfo>(vocabulary_info);
+    instance_info->add_atom("at", {"p1", "A"});
+    instance_info->add_atom("at", {"p1", "B"});
+    instance_info->add_atom("at", {"p2", "A"});
+    instance_info->add_atom("at", {"p1", "B"});
+    instance_info->add_atom("at", {"p3", "A"});
+    instance_info->add_atom("at", {"p1", "B"});
+    instance_info->add_static_atom("package", {"p1"});
+    instance_info->add_static_atom("package", {"p2"});
+    instance_info->add_static_atom("package", {"p3"});
+    return instance_info;
 }
 
 SyntacticElementFactory construct_syntactic_element_factory(std::shared_ptr<const VocabularyInfo> vocabulary_info) {
--- tests/policy/utils.h
+++ tests/policy/utils.h
@@ -6,4 +6,6 @@ using namespace dlplan::policy;
 
 extern std::shared_ptr<const VocabularyInfo> construct_vocabulary_info();
 
+extern std::shared_ptr<const InstanceInfo> construct_instance_info(std::shared_ptr<const VocabularyInfo> vocabulary_info);
+
 extern SyntacticElementFactory construct_syntactic_element_factory(std::shared_ptr<const VocabularyInfo> vocabulary_info);
