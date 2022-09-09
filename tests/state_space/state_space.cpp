f9450bcb6105431bf084cc085716accdbeaf01ff
Dominik Drexler <dominik.drexler@gmx.net>
2022-09-09 15:50:57 +0200

    added state space test

---
+++ tests/state_space/state_space.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+
+#include "../include/dlplan/state_space.h"
+
+using namespace dlplan::core;
+using namespace dlplan::state_space;
+
+
+TEST(DLPTests, StateSpaceTest) {
+    StateSpaceGenerator().generate_state_space_data("domain.pddl", "p-1-0.pddl", "output.txt");
+}
