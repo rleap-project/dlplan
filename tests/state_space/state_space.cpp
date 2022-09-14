a567e267874c5262ce5d2a61837efc2331d50910
Dominik Drexler <dominik.drexler@liu.se>
2022-09-14 16:22:21 +0200

    fixed state space test

--- tests/state_space/state_space.cpp
+++ tests/state_space/state_space.cpp
@@ -7,7 +7,8 @@ using namespace dlplan::state_space;
 
 
 TEST(DLPTests, StateSpaceTest) {
-    auto state_space = StateSpaceGenerator().generate_state_space("domain.pddl", "instance.pddl");
+    StateSpaceGenerator().generate_state_space("domain.pddl", "instance.pddl");
+    auto state_space = StateSpaceReader().read();
     auto distance_information = state_space.compute_goal_distance_information();
     state_space.print();
     ASSERT_EQ(distance_information.is_solvable(), true);
