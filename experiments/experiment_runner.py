2ebb2aaea13882765b65f381be6f73b1720f6d92
Dominik Drexler <dominik.drexler@gmx.net>
2022-09-20 11:32:52 +0200

    changed account

--- experiments/experiment_runner.py
+++ experiments/experiment_runner.py
@@ -39,7 +39,7 @@ if REMOTE:
         memory_per_cpu="3G",
         cpus_per_task=16,
         setup=TetralithEnvironment.DEFAULT_SETUP,
-        extra_options="#SBATCH --account=snic2021-5-330")
+        extra_options="#SBATCH --account=snic2022-5-341")
     SUITE = ["barman", "blocksworld_3", "blocksworld_4", "childsnack", "delivery", "gripper", "miconic", "reward", "spanner", "visitall"]
     TIME_LIMIT = 3 * 3600
 else:
