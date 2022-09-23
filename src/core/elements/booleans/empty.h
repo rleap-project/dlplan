6d3c4879cf04a620e6c73027942a132fc1049a94
Dominik Drexler <dominik.drexler@gmx.net>
2022-09-23 13:45:51 +0200

    .

--- src/core/elements/booleans/empty.h
+++ src/core/elements/booleans/empty.h
@@ -29,7 +29,7 @@ public:
         return denotation;
     }
 
-    std::vector<bool>& evaluate(const States& states, DenotationsCaches& caches) const override {
+    std::vector<bool>* evaluate(const States& states, DenotationsCaches& caches) const override {
         // check if denotations is cached.
         auto cached = caches.m_b_denots_mapping.find(get_index());
         if (cached != caches.m_b_denots_mapping.end()) return cached->second;
--- src/core/elements/element.h
+++ src/core/elements/element.h
@@ -71,9 +71,30 @@ struct DenotationsCaches {
     std::unordered_map<int, std::vector<int>*> m_n_denots_mapping;
     std::unordered_map<int, std::vector<ConceptDenotation*>*> m_c_denots_mapping;
     std::unordered_map<int, std::vector<RoleDenotation*>*> m_r_denots_mapping;
+    // Mapping from instance
+    // std::unordered_map<std::array<int, 3>, std::vector<bool>*> m_b_denots_mapping_per_state;
+};
+
+/*
+struct DenotationsCaches {
+    // Cache for single denotations.
+    dlplan::utils::PointerCache<ConceptDenotation> m_c_denot_cache;
+    dlplan::utils::PointerCache<RoleDenotation> m_r_denot_cache;
+    // Cache for collections of denotations.
+    dlplan::utils::PointerCache<std::vector<bool>> m_b_denots_cache;
+    dlplan::utils::PointerCache<std::vector<int>> m_n_denots_cache;
+    // std::reference_wrapper to indicate not null? We would need custom equal operator
+    dlplan::utils::PointerCache<std::vector<std::reference_wrapper<const ConceptDenotation>>> m_c_denots_cache;
+    dlplan::utils::PointerCache<std::vector<std::reference_wrapper<const RoleDenotation>>> m_r_denots_cache;
+    // Mapping from element index to denotations.
+    std::unordered_map<int, std::reference_wrapper<const std::vector<bool>>> m_b_denots_mapping;
+    std::unordered_map<int, std::reference_wrapper<const std::vector<int>>> m_n_denots_mapping;
+    std::unordered_map<int, std::reference_wrapper<const std::vector<std::reference_wrapper<const ConceptDenotation>>>> m_c_denots_mapping;
+    std::unordered_map<int, std::reference_wrapper<const std::vector<std::reference_wrapper<const RoleDenotation>>>> m_r_denots_mapping;
     // Mapping from instance, state, element index to denotations
     // std::unordered_map<std::array<int, 3>, std::vector<bool>*> m_b_denots_mapping_per_state;
 };
+*/
 
 }
 
