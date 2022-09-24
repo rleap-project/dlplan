c1f2a58ac6f7b560a2e3401e8be1e4b27d4e13d9
Dominik Drexler <dominik.drexler@liu.se>
2022-09-24 14:41:57 +0200

    added empty generator tests

---
+++ tests/generator/generator.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+
+#include <iostream>
+#include <vector>
+
+#include "../include/dlplan/novelty.h"
+
+
+using namespace dlplan::novelty;
+
+TEST(DLPTests, DeliveryTest) {
+
+}
