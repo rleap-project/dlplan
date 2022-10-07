028e505713def05bd97a2aa2d4f5d0bcf8f1adff
Dominik Drexler <dominik.drexler@gmx.net>
2022-10-07 14:02:12 +0200

    added functionality to novelty

--- src/novelty/novelty_table.cpp
+++ src/novelty/novelty_table.cpp
@@ -3,8 +3,8 @@
 
 namespace dlplan::novelty {
 
-NoveltyTable::NoveltyTable(std::shared_ptr<const NoveltyBase> novelty_base)
-    : m_novelty_base(novelty_base) { }
+NoveltyTable::NoveltyTable(int num_tuples)
+    : m_table(num_tuples, true) { }
 
 NoveltyTable::NoveltyTable(const NoveltyTable& other) = default;
 
@@ -15,5 +15,20 @@ NoveltyTable::NoveltyTable(NoveltyTable&& other) = default;
 NoveltyTable& NoveltyTable::operator=(NoveltyTable&& other) = default;
 
 NoveltyTable::~NoveltyTable() = default;
+
+bool NoveltyTable::insert(TupleIndexGenerator&& tuple_index_generator, bool stop_if_novel) {
+    bool result = false;
+    for (auto tuple_index : tuple_index_generator) {
+        bool is_novel = m_table[tuple_index];
+        if (!result && is_novel) {
+            result = true;
+        }
+        m_table[tuple_index] = false;
+        if (stop_if_novel) {
+            break;
+        }
+    }
+    return result;
+}
 
 }
