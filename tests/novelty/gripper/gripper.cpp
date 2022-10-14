daf7fa28072dc7731fbc51fceafdf3270d9f8fcd
Dominik Drexler <dominik.drexler@liu.se>
2022-10-14 09:08:12 +0200

    remove dead code

--- tests/novelty/gripper/gripper.cpp
+++ tests/novelty/gripper/gripper.cpp
@@ -13,7 +13,6 @@ using namespace dlplan::novelty;
 TEST(DLPTests, NoveltyGripperTest) {
     StateSpaceGenerator().generate_state_space("domain.pddl", "p-1-0.pddl");
     auto state_space = StateSpaceReader().read();
-    auto distance_information = state_space.compute_goal_distance_information();
     state_space.print();
     auto novelty_base = std::make_shared<NoveltyBase>(
         state_space.get_instance_info_ref().get_atoms_ref().size(),
