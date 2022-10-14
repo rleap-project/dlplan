bcaab42d90a52f33958be682342c85a3d568128d
Dominik Drexler <dominik.drexler@liu.se>
2022-10-14 09:14:21 +0200

    added printing atoms

--- tests/novelty/gripper/gripper.cpp
+++ tests/novelty/gripper/gripper.cpp
@@ -13,12 +13,18 @@ using namespace dlplan::novelty;
 TEST(DLPTests, NoveltyGripperTest) {
     StateSpaceGenerator().generate_state_space("domain.pddl", "p-1-0.pddl");
     auto state_space = StateSpaceReader().read();
+    std::cout << "State space:" << std::endl;
     state_space.print();
     auto novelty_base = std::make_shared<NoveltyBase>(
         state_space.get_instance_info_ref().get_atoms_ref().size(),
         1);
+    std::cout << std::endl << "Atoms:" << std::endl;
+    for (const auto& atom : state_space.get_instance_info_ref().get_atoms_ref()) {
+        std::cout << atom.get_index() << " " << atom.get_name_ref() << std::endl;
+    }
+    std::cout << std::endl << "Tuple graphs:" << std::endl;
     for (const auto state_index : state_space.get_state_indices_ref()) {
-        auto tuple_graph = TupleGraph(novelty_base, state_space, state_index, 0);
+        auto tuple_graph = TupleGraph(novelty_base, state_space, state_index, 1);
         std::cout << tuple_graph.str() << std::endl << std::endl;
     }
 }
