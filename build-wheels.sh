b15d70a3bc482eae82c473a359bc8a45e738eeeb
Dominik Drexler <dominik.drexler@gmx.net>
2023-04-26 12:19:58 +0200

    some work on building wheels. the problems seems to be that cmake finds wrong intepreter, i.e., does not match the target python version

--- build-wheels.sh
+++ build-wheels.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-yum install -y cmake python3 python3-devel python3-pip7
-yum groupinstall -y "Development Tools"
-
-
-# Build wheels for Python 3.6, 3.7, 3.8, 3.9 (adjust the list as needed)
-for PYTHON in cp36-cp36 cp37-cp37 cp38-cp38 cp39-cp39 cp310-cp310 cp311-cp311; do
-    /opt/python/${PYTHON}/bin/pip install -r requirements.txt
-    /opt/python/${PYTHON}/bin/python setup.py bdist_wheel
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" install -r /io/dev-requirements.txt
+    "${PYBIN}/pip" wheel /io/ --no-deps -w wheelhouse/
 done
-
 
 # Repair the generated wheels to make them manylinux-compliant
 for whl in dist/*.whl; do
