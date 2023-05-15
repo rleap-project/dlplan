4d853deb8833abfcf1677c4b217f7de7ed7b6adc
Dominik Drexler <dominik.drexler@gmx.net>
2023-05-15 15:13:09 +0200

    added missing fiels

---
+++ tests/utils/domain.h
@@ -0,0 +1,24 @@
+#ifndef DLPLAN_TESTS_UTILS_DOMAIN_H_
+#define DLPLAN_TESTS_UTILS_DOMAIN_H_
+
+
+#include "../../include/dlplan/policy.h"
+
+using namespace dlplan::core;
+using namespace dlplan::policy;
+
+namespace gripper {
+extern std::shared_ptr<const VocabularyInfo> construct_vocabulary_info();
+
+extern std::shared_ptr<const VocabularyInfo> construct_blocks_vocabulary_info();
+
+extern std::shared_ptr<const InstanceInfo> construct_instance_info(std::shared_ptr<const VocabularyInfo> vocabulary_info);
+}
+
+namespace blocks_4 {
+
+}
+
+extern SyntacticElementFactory construct_syntactic_element_factory(std::shared_ptr<const VocabularyInfo> vocabulary_info);
+
+#endif
