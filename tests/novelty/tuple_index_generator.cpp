ae085e4eb75789c55a8f4be32bcef003950ded8d
Dominik Drexler <dominik.drexler@gmx.net>
2023-06-08 21:18:08 +0200

    removed old tuple_index_iterator<2> implementation

--- tests/novelty/tuple_index_generator.cpp
+++ tests/novelty/tuple_index_generator.cpp
@@ -38,7 +38,7 @@ TEST(DLPTests, TupleIndexGeneratorArity1Test) {
 TEST(DLPTests, TupleIndexGeneratorArity2Test) {
     // Test with single atom tuple.
     auto novelty_base = std::make_shared<const NoveltyBase>(4, 2);
-    auto tuple_index_generator = TupleIndexGenerator(novelty_base, {}, {0,1});
+    auto tuple_index_generator = TupleIndexGenerator<2>(novelty_base, {}, {0,1});
     auto begin = tuple_index_generator.begin();
     auto end = tuple_index_generator.end();
     EXPECT_EQ(*(begin++), 5);  // {0,1} 0*5^0 + 1*5^1
@@ -48,7 +48,7 @@ TEST(DLPTests, TupleIndexGeneratorArity2Test) {
     EXPECT_EQ(begin, end);
 
     // Test with unsorted atom indices, we get the same result
-    tuple_index_generator = TupleIndexGenerator(novelty_base, {}, {1,0});
+    tuple_index_generator = TupleIndexGenerator<2>(novelty_base, {}, {1,0});
     begin = tuple_index_generator.begin();
     end = tuple_index_generator.end();
     EXPECT_EQ(*(begin++), 5);  // {0,1} 5 = 0*5^0 + 1*5^1
@@ -58,14 +58,14 @@ TEST(DLPTests, TupleIndexGeneratorArity2Test) {
     EXPECT_EQ(begin, end);
 
     // Test with empty atom indices.
-    tuple_index_generator = TupleIndexGenerator(novelty_base, {}, {});
+    tuple_index_generator = TupleIndexGenerator<2>(novelty_base, {}, {});
     begin = tuple_index_generator.begin();
     end = tuple_index_generator.end();
     EXPECT_EQ(*(begin++), 24);  // {4,4} 4*5^0 + 4*5^1
     EXPECT_EQ(begin, end);
 
     // Test for more than one atom tuple.
-    tuple_index_generator = TupleIndexGenerator(novelty_base, {}, {0,1,2});
+    tuple_index_generator = TupleIndexGenerator<2>(novelty_base, {}, {0,1,2});
     begin = tuple_index_generator.begin();
     end = tuple_index_generator.end();
     EXPECT_EQ(*(begin++), 5);  // {0,1} 0*5^0 + 1*5^1
