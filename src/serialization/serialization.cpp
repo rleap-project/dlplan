f6be804cadea4b39d7b17eaa6277ebef0aad9751
Dominik Drexler <dominik.drexler@gmx.net>
2023-07-26 15:05:35 +0200

    added empty serialize function

--- src/serialization/serialization.cpp
+++ src/serialization/serialization.cpp
@@ -1,8 +1,51 @@
 #include "../../include/dlplan/serialization.h"
 
+#include "../../include/dlplan/core.h"
+#include "../../include/dlplan/state_space.h"
+
 #include <boost/serialization/base_object.hpp>
+#include <boost/serialization/shared_ptr.hpp>
+
+
+using namespace dlplan;
+
 
 namespace dlplan::serialization {
+
+template<typename Archive>
+void serialize( Archive& ar, core::Predicate& predicate, const unsigned int version )
+{
+}
+
+template<typename Archive>
+void serialize( Archive& ar, core::Constant& constant, const unsigned int version )
+{
+}
+
+template<typename Archive>
+void serialize( Archive& ar, core::Object& object, const unsigned int version )
+{
+}
+
+template<typename Archive>
+void serialize( Archive& ar, core::Atom& atom, const unsigned int version )
+{
+}
+
+template<typename Archive>
+void serialize( Archive& ar, std::shared_ptr<core::VocabularyInfo>& vocabulary_info, const unsigned int version )
+{
+}
+
+template<typename Archive>
+void serialize( Archive& ar, std::shared_ptr<core::InstanceInfo>& instance_info, const unsigned int version )
+{
+}
+
+template<typename Archive>
+void serialize( Archive& ar, std::unique_ptr<state_space::StateSpace>& state_space, const unsigned int version )
+{
+}
 
 
 void serialize(const Data& data, std::ostream& out_buffer) {
