2a09df444720fffc67638fbf909cd37023f00ab3
Dominik Drexler <dominik.drexler@gmx.net>
2023-08-07 12:10:28 +0200

    .

--- .github/workflows/scripts/install_boost.sh
+++ .github/workflows/scripts/install_boost.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # yum install -y wget
-pwd
-cd /
+ROOT_DIR=$(pwd)
 mkdir boost
 cd boost
 wget --no-check-certificate 'https://boostorg.jfrog.io/artifactory/main/release/1.82.0/source/boost_1_82_0.tar.gz'
@@ -10,4 +9,5 @@ tar xf boost_1_82_0.tar.gz
 cd boost_1_82_0
 ./bootstrap.sh
 ./b2
-export BOOST_ROOT="/boost/boost_1_82_0"
+cd "${ROOT_DIR}"
+export BOOST_ROOT="${ROOT_DIR}/boost/boost_1_82_0"
