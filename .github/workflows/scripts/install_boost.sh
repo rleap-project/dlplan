f99051f473f383350bb34ea5baa14b0ebb3b6398
Dominik Drexler <dominik.drexler@gmx.net>
2023-08-07 13:27:13 +0200

    .

--- .github/workflows/scripts/install_boost.sh
+++ .github/workflows/scripts/install_boost.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-CWD=$1
-
-cd ${CWD}
-mkdir boost
-cd boost
 wget --no-check-certificate 'https://boostorg.jfrog.io/artifactory/main/release/1.82.0/source/boost_1_82_0.tar.gz'
 tar xf boost_1_82_0.tar.gz
 cd boost_1_82_0
@@ -13,6 +8,3 @@ cd boost_1_82_0
 # Compile with fPIC flag
 export CXXFLAGS="-fPIC"
 ./b2 cxxflags="$CXXFLAGS"
-
-export BOOST_ROOT="${CWD}/boost/boost_1_82_0"
-cd ${CWD}
