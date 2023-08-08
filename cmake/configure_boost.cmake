7702c5fd166594bc2bd2548d2ac680d1a6420ba6
Dominik Drexler <dominik.drexler@gmx.net>
2023-08-08 13:40:41 +0200

    added macro to configure cmake

---
+++ cmake/configure_boost.cmake
@@ -0,0 +1,12 @@
+
+macro(configure_boost)
+    set(Boost_USE_STATIC_LIBS ON)
+    set(Boost_USE_MULTITHREADED ON)
+    set(Boost_USE_STATIC_RUNTIME OFF)
+    set(BOOST_MIN_VERSION "1.80.0")
+
+    if (DEFINED ENV{BOOST_ROOT})
+        set(Boost_NO_SYSTEM_PATHS ON)
+        set(BOOST_ROOT $ENV{BOOST_ROOT})
+    endif()
+endmacro()
