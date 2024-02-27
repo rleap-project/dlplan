6e6328911a20bf975d0cb0a847e2686bf169e353
Dominik Drexler <dominik.drexler@gmx.net>
2024-02-27 15:22:19 +0100

    lowered boost min version

--- cmake/configure_boost.cmake
+++ cmake/configure_boost.cmake
@@ -3,7 +3,7 @@ macro(configure_boost)
     set(Boost_USE_STATIC_LIBS ON)
     set(Boost_USE_MULTITHREADED ON)
     set(Boost_USE_STATIC_RUNTIME OFF)
-    set(BOOST_MIN_VERSION "1.80.0")
+    set(BOOST_MIN_VERSION "1.74.0")
 
     if (DEFINED ENV{BOOST_ROOT})
         set(Boost_NO_SYSTEM_PATHS ON)
